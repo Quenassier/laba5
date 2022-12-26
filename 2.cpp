4bdfc1c679844cb490a46416061cb42110f67fc9
Quenassier <112952435+Quenassier@users.noreply.github.com>
2022-12-26 23:28:47 +0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,32 +1,43 @@
 #include <iostream>
-#define N 3
-int main() {
+#include <math.h>
+void inputArray();
+int main(){
+    inputArray();
+}
+void inputArray() { 
+    const int arraySize = 10;
+    int array[arraySize][arraySize];
+    int up = 0;
+    int right = 0;
+    int dawn = 0;
+    int left = 0;
+    int value = 1;
 
-    int cases = 1;
-    int j = 1;
-    int i = 0;
-    int arr[N][N];
-
-    for (i; i < N; i++) {
-        arr[0][i] = cases;
-        cases++;
+    while (value <= pow(arraySize, 2)) {
+        for (int i = up; i < arraySize - up; i++) {
+            array[up][i] = value;                              
+            value++;
+        }
+        up++;               
+        for (int i = right + 1; i < arraySize - right; i++) {
+            array[i][arraySize - right - 1] = value;           
+            value++;
+        }
+        right++;                
+        for (int i = arraySize - 1 - right; i >= left; i--) {
+           array[arraySize - 1 - dawn][i] = value;
+           value++;
+        }
+        dawn++;
+        for (int i = arraySize - 1 - dawn; i >= up; i--) {
+            array[i][left] = value;                              
+            value++;
+        }
+        left++;          
     }
-    for (j; j < N; j++) {
-        arr[j][i - 1] = cases;
-        cases++;
-    }
-    for (int i = N - 2; i >= 0; i--) {
-        arr[j - 1][i] = cases;
-        cases++;
-    }
-    for (int i = 0; i <= N - 2; i++) {
-        arr[1][i] = cases;
-        cases++;
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            std::cout << arr[i][j];
-
+    for (int i = 0; i < arraySize; i++) {
+        for (int j = 0; j < arraySize; j++) {
+            std::cout << array[i][j] << ' ' << ' ';
         }
         std::cout << '\n';
     }
